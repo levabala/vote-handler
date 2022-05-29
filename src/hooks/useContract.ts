3f682b0c766bae44c72890566dbebeb7417c57b5
levabala <levabala@ya.ru>
2022-05-29 20:16:14 +0300

    chore: add react with parcel and migrate to yarn

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,6 +1,9 @@
-import "dotenv/config";
 import Web3 from "web3";
 import Tx from "@ethereumjs/tx";
 
 const rpcURL = `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
 const web3 = new Web3(rpcURL);
+
+export function useContract() {
+  console.log(web3);
+}
